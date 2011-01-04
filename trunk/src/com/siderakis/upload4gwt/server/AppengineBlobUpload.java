901736af391b07a6d95f3119406a70a34ac64688
nixsid@gmail.com <nixsid@gmail.com@2dee3fef-ae61-3fec-351a-20cf58d1ee72>
2011-01-04 01:13:25 +0000

    add blob upload

---
+++ trunk/src/com/siderakis/upload4gwt/server/AppengineBlobUpload.java
@@ -0,0 +1,34 @@
+package com.siderakis.upload4gwt.server;
+
+import java.io.IOException;
+import java.util.Map;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+
+public class AppengineBlobUpload extends HttpServlet {
+	private final BlobstoreService blobstoreService = BlobstoreServiceFactory
+	.getBlobstoreService();
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void doPost(final HttpServletRequest req,
+			final HttpServletResponse res) throws ServletException, IOException {
+
+		final Map<String, BlobKey> blobs = blobstoreService
+		.getUploadedBlobs(req);
+		final BlobKey blobKey = blobs.get("myFile");
+
+		if (blobKey == null) {
+			res.sendRedirect("/");
+		} else {
+			res.sendRedirect("/serve?blob-key=" + blobKey.getKeyString());
+		}
+	}
+}
