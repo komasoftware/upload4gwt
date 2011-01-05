9c3798fc45376a2b8859aae1150132a5b69c89cd
nixsid <nixsid@gmail.com>
2011-01-05 15:00:34 +0000

    update analytics and logs

--- src/com/siderakis/upload4gwt/server/AppengineListener.java
+++ src/com/siderakis/upload4gwt/server/AppengineListener.java
@@ -7,8 +7,7 @@ import org.apache.commons.fileupload.ProgressListener;
 
 import com.google.inject.servlet.SessionScoped;
 
-@SessionScoped
-public class AppengineListener implements ProgressListener, Serializable {
+@SessionScoped public class AppengineListener implements ProgressListener, Serializable {
 
 	private static final long serialVersionUID = 1L;
 
@@ -18,27 +17,25 @@ public class AppengineListener implements ProgressListener, Serializable {
 	private static final Logger log = Logger.getLogger(AppengineListener.class.getName());
 
 	public Integer getPercentage() {
-		log.info("pContentLength:bytesRead::" + contentLength + ":" + bytesRead);
+		log.warning("pContentLength:bytesRead::" + contentLength + ":" + bytesRead);
 		return contentLength.equals(0L) ? 0 : Math.round(bytesRead * 100 / contentLength);
 
 	}
 
-	@Override
-	public void update(final long bytesRead, final long contentLength, final int items) {
+	@Override public void update(final long bytesRead, final long contentLength, final int items) {
 
 		this.bytesRead = bytesRead;
 		this.contentLength = contentLength;
-		log.info("We are currently reading item " + items);
+		log.warning("We are currently reading item " + items);
 		if (contentLength == -1) {
-			log.info("So far, " + bytesRead + " bytes have been read.");
+			log.warning("So far, " + bytesRead + " bytes have been read.");
 		} else {
-			log.info("So far, " + bytesRead + " of " + contentLength + " bytes have been read.");
+			log.warning("So far, " + bytesRead + " of " + contentLength + " bytes have been read.");
 		}
 		if (sleepMilliseconds > 0 && bytesRead < contentLength) {
 			try {
 				Thread.sleep(sleepMilliseconds);
-			} catch (Exception e) {
-			}
+			} catch (final Exception e) {}
 		}
 
 	}
