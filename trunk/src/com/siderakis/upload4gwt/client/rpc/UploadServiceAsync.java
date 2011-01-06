a94d097b5e69bda6f932f24b63c2ba82942ab895
nixsid@gmail.com <nixsid@gmail.com@2dee3fef-ae61-3fec-351a-20cf58d1ee72>
2011-01-06 20:05:14 +0000

    updated uploadserver to send uploadstatus objects

--- trunk/src/com/siderakis/upload4gwt/client/rpc/UploadServiceAsync.java
+++ trunk/src/com/siderakis/upload4gwt/client/rpc/UploadServiceAsync.java
@@ -8,4 +8,6 @@ import com.siderakis.upload4gwt.shared.UploadStatus;
 public interface UploadServiceAsync {
 	void getUploadStatus(List<Long> uploadIds, AsyncCallback<List<UploadStatus>> callback)
 			throws IllegalArgumentException;
+
+	void getInitialUploadId(AsyncCallback<String> callback);
 }
