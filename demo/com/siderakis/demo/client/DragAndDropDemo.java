60095627713eebaca124ad1c770dd916cb4f37b8
nixsid <nixsid@gmail.com>
2011-07-09 13:42:02 +0000

    Extracted common layout code into new DemoComposite class.

--- demo/com/siderakis/demo/client/DragAndDropDemo.java
+++ demo/com/siderakis/demo/client/DragAndDropDemo.java
@@ -16,20 +16,17 @@
 package com.siderakis.demo.client;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Anchor;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Label;
+import com.siderakis.demo.client.Upload4gwt.DemoComposite;
 import com.siderakis.upload4gwt.client.dnd.UploadDropTarget;
 import com.siderakis.upload4gwt.client.ui.SimpleProgressBar;
 
-public class DragAndDropDemo extends Composite {
+public class DragAndDropDemo extends DemoComposite {
 	private static final String UPLOAD_ACTION_URL = GWT.getModuleBaseURL() + "upload";
 
 	public DragAndDropDemo() {
-		final FlowPanel panel = new FlowPanel();
-		panel.setStyleName("panel");
-		panel.add(new HTML("Upload4gwt"));
 
 		final FlowPanel dropPanel = new FlowPanel();
 		dropPanel.addStyleName("dropPanel");
@@ -40,8 +37,8 @@ public class DragAndDropDemo extends Composite {
 		panel.add(simpleProgressBar);
 		new UploadDropTarget().uploader(dropPanel.getElement(), UPLOAD_ACTION_URL, simpleProgressBar);
 
-		final FlowPanel outer = new FlowPanel();
-		outer.add(panel);
-		initWidget(outer);
+		outer.add(new Anchor("See source",
+		"http://code.google.com/p/upload4gwt/source/browse/trunk/demo/com/siderakis/demo/client/DragAndDropDemo.java"));
+
 	}
 }
